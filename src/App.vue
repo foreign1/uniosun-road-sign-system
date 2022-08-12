b8bd083d4d0839f7035a76893361e15c295bf02d
foreign1 <32528727+foreign1@users.noreply.github.com>
2022-08-12 07:29:27 +0100

    silent-window: update sass compile directory and injection point

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <!-- <language-selector v-if="language === ''" @languageSelected="setUserLanguage" :refresh='refresh'/>
-    <mode-selector v-else-if="mode === ''" :interaction="interaction" @modeSelected="setMode" :refresh='refresh' />
-    <text-mode v-else-if="mode === 'text' && destination === ''" :destinationQuery="destinationQuery" @destinationSet="setDestination" :refresh='refresh'/>
-    <search-result v-else :destination="destination" :refresh='refresh' /> -->
-    <!-- <router-view/> -->
   </div>
 </template>
 <script>
-import '../dist/style.css'
 export default {
   name: 'App',
 }
